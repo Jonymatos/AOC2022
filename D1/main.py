28f6fdff06826087f0a98a43641a7e05228d517b
João Rui Matos <joaoruimatos@gmail.com>
2022-12-01 18:39:23 +0000

    1st commit on AOC

---
+++ D1/main.py
@@ -0,0 +1,14 @@
+buffer = []
+total = []
+with open("input.txt", "r") as f:
+	lst = [el.strip() for el in f.readlines()]
+	for num in lst:
+		if num == "":
+			total.append(sum(buffer))
+			buffer = []
+		else:
+			buffer.append(int(num))
+
+# Write "1" for Part1 and "3" for Part2
+top = int(eval(input("Enter the top number: ")))
+print(sum(sorted(total, reverse = True)[:top]))
