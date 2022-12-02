bfa9639cfc0503ba27631414f8d7be18cb1de801
João Rui Matos <joaoruimatos@gmail.com>
2022-12-02 12:55:59 +0000

    Minor improvement on Day2

--- D2/d2.py
+++ D2/d2.py
@@ -1,11 +1,11 @@
 def part1(play):
-	points = ord(play[1]) % 3 if ord(play[1]) % 3 != 0 else 3
+	points = (ord(play[1]) - 1) % 3 + 1
 
 	if ord(play[0]) % 3 == ord(play[1]) % 3: #Win
 		return points + 6
 	if (ord(play[0]) - 1) % 3 == ord(play[1]) % 3: #Draw
 		return points + 3
-		
+
 	return points #Lose
 
 def part2(play):
@@ -19,7 +19,7 @@ def part2(play):
 	return result + (ord(play[0]))  % 3 + 1 #Lose
 
 
-with open("input.txt", "r") as f:
+with open("example.txt", "r") as f:
 	lst = [el.strip().split() for el in f.readlines()]
 	print(sum(list(map(part1, lst)))) # Part1
 	print(sum(list(map(part2, lst)))) # Part2
