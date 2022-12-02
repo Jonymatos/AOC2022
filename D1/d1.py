f6ea0c3c0bbb10b6c4a722b8fb615733cc2bc368
João Rui Matos <joaoruimatos@gmail.com>
2022-12-02 15:42:22 +0000

    Fix extra indentation, minor improvement on Day2

--- D1/d1.py
+++ D1/d1.py
@@ -1,13 +1,13 @@
 buffer = []
 total = []
 with open("input.txt", "r") as f:
-	lst = [el.strip() for el in f.readlines()]
-	for num in lst:
-		if num == "":
-			total.append(sum(buffer))
-			buffer = []
-		else:
-			buffer.append(int(num))
+    lst = [el.strip() for el in f.readlines()]
+    for num in lst:
+        if num == "":
+            total.append(sum(buffer))
+            buffer = []
+        else:
+            buffer.append(int(num))
 
 # Write "1" for Part1 and "3" for Part2
 top = int(eval(input("Enter the top number: ")))
--- D2/d2.py
+++ D2/d2.py
@@ -1,25 +1,25 @@
 def part1(play):
-	points = (ord(play[1]) - 1) % 3 + 1
+    points = (ord(play[1]) - 1) % 3 + 1
 
-	if ord(play[0]) % 3 == ord(play[1]) % 3: #Win
-		return points + 6
-	if (ord(play[0]) - 1) % 3 == ord(play[1]) % 3: #Draw
-		return points + 3
+    if ord(play[0]) % 3 == ord(play[1]) % 3: #Win
+        return points + 6
+    if (ord(play[0]) - 1) % 3 == ord(play[1]) % 3: #Draw
+        return points + 3
 
-	return points #Lose
+    return points #Lose
 
 def part2(play):
-	result = (ord(play[1]) - 1) % 3 * 3
+    result = (ord(play[1]) - 1) % 3 * 3
 
-	if result == 6: #Win
-		return result + (ord(play[0]) - 1)  % 3 + 1
-	if result == 3: #Draw
-		return result + (ord(play[0]) + 1) % 3 + 1
+    if result == 6: #Win
+        return result + (ord(play[0]) - 1) % 3 + 1
+    if result == 3: #Draw
+        return result + (ord(play[0]) + 1) % 3 + 1
 
-	return result + (ord(play[0]))  % 3 + 1 #Lose
+    return ord(play[0]) % 3 + 1 #Lose, result is 0
 
 
-with open("example.txt", "r") as f:
-	lst = [el.strip().split() for el in f.readlines()]
-	print(sum(list(map(part1, lst)))) # Part1
-	print(sum(list(map(part2, lst)))) # Part2
+with open("input.txt", "r") as f:
+    lst = [el.strip().split() for el in f.readlines()]
+    print(sum(list(map(part1, lst)))) # Part1
+    print(sum(list(map(part2, lst)))) # Part2
