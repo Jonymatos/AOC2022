9a8b568afec26014155e802dfcb716f3a824e72f
João Rui Matos <joaoruimatos@gmail.com>
2022-12-04 15:47:36 +0000

    Tbh not sure if it's optimized

--- D4/d4.py
+++ D4/d4.py
@@ -1,14 +1,14 @@
 def verify1(pair):
-    return (int(pair[0][0]) <= int(pair[1][0]) and int(pair[0][1]) >= int(pair[1][1])) or\
-        (int(pair[0][0]) >= int(pair[1][0]) and int(pair[0][1]) <= int(pair[1][1]))
+    return (pair[0][0] <= pair[1][0] and pair[0][1] >= pair[1][1]) or\
+        (pair[0][0] >= pair[1][0] and pair[0][1] <= pair[1][1])
 
 def verify2(pair):
-    return (int(pair[0][0]) <= int(pair[1][1]) and int(pair[0][1]) >= int(pair[1][0])) or\
-        (int(pair[0][0]) >= int(pair[1][1]) and int(pair[0][1]) <= int(pair[1][0]))  
+    return (pair[0][0] <= pair[1][1] and pair[0][1] >= pair[1][0]) or\
+        (pair[0][0] >= pair[1][1] and pair[0][1] <= pair[1][0])
 
 with open("input.txt", "r") as f:
     lst = [el.strip().split(',') for el in f.readlines()]
-    lst = list(map(lambda x: [x[0].split('-'), x[1].split('-')], lst))
+    lst = list(map(lambda x: [list(map(int, x[0].split('-'))), list(map(int, x[1].split('-')))], lst))
     print(len(list(filter(verify1, lst))))
     print(len(list(filter(verify2, lst))))
     
